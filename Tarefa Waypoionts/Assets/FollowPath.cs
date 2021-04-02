9a14a17cc615d828bd848e0537700b77ea2fe2ae
André Sena <Curi_asa@hotmail.com>
2021-04-02 02:42:24 -0300

    commit atividades waypoints

---
+++ Tarefa Waypoionts/Assets/FollowPath.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowPath : MonoBehaviour
+{
+    //Variaveis Globais do Tank
+
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start()
+    {
+        //wps variavel de gerenciamento dos pontos de locomoçao do tank
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        //g a variavel de valores de posiçoes(x, y, z) no mapa
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        currentNode = g.getPathPoint(currentWP);
+        //condição de seguimento dos waypoints
+        if(Vector3.Distance(
+            g.getPathPoint(currentWP).transform.position,
+            transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+        //condição da direção que o tank esta olhando e giro do seu eixo frontal
+        if(currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        }
+        //adicinando a velocidade para o tank 
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    //funçao de botao para movimentar o tank para a Heliporto no wp 1
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+
+    //funçao de botao para movimentar o tank para a ruina no wp 6
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+    }
+}
---
+++ Tarefa Waypoionts/Assets/WPManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+// estrutura basica de coordernada e direçao apontada para o tank
+public struct Link
+{
+    public enum direction { UNI,BI}
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    // instanciando todas as parametrizaçoes de variaveis
+    public GameObject[] waypoints;
+    public Link[] links;
+    public Graph graph = new Graph();
+
+    void Start()
+    {
+        //definindo que precisa existir pelo menos 1 waypoint
+        if (waypoints.Length>0)
+        {
+            //processo de repetição para entrada dos waypoints no node
+            foreach(GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            //processo de repetiçao para todos pontos que formam a rota nos nodes1 e node2 do link 
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if (l.dir == Link.direction.BI)
+                    graph.AddEdge(l.node2, l.node1);
+            }
+
+        }
+        
+    }
+
+    
+    void Update()
+    {
+        //linhas mapeadas da rota em modo de Debug
+        graph.debugDraw();
+    }
+}
